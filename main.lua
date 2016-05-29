140984c53e8b207631ce9d2af57095095725e04f
Rami Sabbagh <ramilego4game@gmail.com>
2016-05-29 13:13:37 +0300

    Now prints some information about the tool

--- main.lua
+++ main.lua
@@ -2,17 +2,19 @@
 io.stdout:setvbuf("no")
 
 local JSON = require("JSON")
+local _Width, _Height = love.graphics.getDimensions()
 local Font = {}
 
 function love.load(arg)
   love.graphics.setBackgroundColor(250,250,250)
-  Font[12] = love.graphics.newFont("NotoSans-Bold.ttf",12)
+  Font[12] = love.graphics.newFont("NotoSans-Regular.ttf",12)
 end
 
 function love.draw()
-  love.graphics.setColor(25,25,25,255)
+  love.graphics.setColor(150,150,150,255)
   love.graphics.setFont(Font[12])
-  love.graphics.printf(text,x,y,limit,align,r,sx,sy,ox,oy,kx,ky)
+  love.graphics.printf("Please drop the sheet files into this window",_Width/8,_Height/4,(_Width/8)*6,"center")
+  love.graphics.printf("This tool has been made by RamiLego4Game for MoveOrDie.",(_Height/16),_Height-(_Height/8),_Width-(_Height/8),"left")
 end
 
 function love.update(dt)
