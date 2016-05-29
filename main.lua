b1920ccfc32e6b3b876f56560d29447b06ecf600
Rami Sabbagh <ramilego4game@gmail.com>
2016-05-29 14:15:31 +0300

    Removed error msg

--- main.lua
+++ main.lua
@@ -3,7 +3,7 @@ io.stdout:setvbuf("no")
 
 local JSON = require("JSON")
 local _Width, _Height = love.graphics.getDimensions()
-local _Font, _Images, _Info, _Status, _Errmsg = {}, {}, "Please drop the sheet files into this window", "Waiting for sheet files...", "----"
+local _Font, _Images, _Info, _Status, _Errmsg = {}, {}, "Please drop the sheet files into this window", "Waiting for sheet files...", ""
 
 function love.load(arg)
   love.graphics.setBackgroundColor(255,255,255)
@@ -14,8 +14,10 @@ function love.draw()
   love.graphics.setFont(_Font[12])
   
   love.graphics.setColor(100,100,100,255)
-  
   love.graphics.printf(_Info.."\n".._Status,_Width/8,_Height/4,(_Width/8)*6,"center")
+  
+  love.graphics.setColor(200,0,0,255)
+  love.graphics.printf(_Errmsg,_Width/8,_Height/2,(_Width/8)*6,"center")
   
   love.graphics.setColor(150,150,150,255)
   love.graphics.printf("This tool has been made by RamiLego4Game for MoveOrDie.",(_Height/16),_Height-(_Height/8),_Width-(_Height/8),"left")
