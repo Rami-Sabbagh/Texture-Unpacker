6093593bb43d2bc2b4e94259ac16b0e12996828e
Rami Sabbagh <ramilego4game@gmail.com>
2016-05-29 12:59:31 +0300

    WIP

--- main.lua
+++ main.lua
@@ -1,13 +1,18 @@
 --TextureUnpacker By RamiLego4Game--
 io.stdout:setvbuf("no")
-local JSON = loadfile("JSON.lua")()
+
+local JSON = require("JSON")
+local Font = {}
 
 function love.load(arg)
   love.graphics.setBackgroundColor(250,250,250)
+  Font[12] = love.graphics.newFont("NotoSans-Bold.ttf",12)
 end
 
 function love.draw()
-  
+  love.graphics.setColor(25,25,25,255)
+  love.graphics.setFont(Font[12])
+  love.graphics.printf(text,x,y,limit,align,r,sx,sy,ox,oy,kx,ky)
 end
 
 function love.update(dt)
