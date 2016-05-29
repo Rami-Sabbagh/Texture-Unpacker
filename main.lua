bdd2ff4b8c6e9db2ad037215da7680e342d1718c
Rami Sabbagh <ramilego4game@gmail.com>
2016-05-29 13:58:58 +0300

    Now loads images... And decodes json files

--- main.lua
+++ main.lua
@@ -3,16 +3,16 @@ io.stdout:setvbuf("no")
 
 local JSON = require("JSON")
 local _Width, _Height = love.graphics.getDimensions()
-local Font, _Info, _Status = {}, "Please drop the sheet files into this window", "Waiting for sheet files..."
+local _Font, _Images, _Info, _Status = {}, {}, "Please drop the sheet files into this window", "Waiting for sheet files..."
 
 function love.load(arg)
   love.graphics.setBackgroundColor(255,255,255)
-  Font[12] = love.graphics.newFont("NotoSans-Regular.ttf",12)
+  _Font[12] = love.graphics.newFont("NotoSans-Regular.ttf",12)
 end
 
 function love.draw()
   love.graphics.setColor(100,100,100,255)
-  love.graphics.setFont(Font[12])
+  love.graphics.setFont(_Font[12])
   love.graphics.printf(_Info.."\n".._Status,_Width/8,_Height/4,(_Width/8)*6,"center")
   
   love.graphics.setColor(150,150,150,255)
@@ -21,11 +21,20 @@ end
 
 local function splitFilePath(path)
   return path:match("(.-)([^\\/]-%.?([^%.\\/]*))$") --("^.+/(.+)$")
-end
+end 
 
 function love.filedropped(file)
   local filePath, fileName, fileExtension = splitFilePath(file:getFilename())
   print(fileName.." ("..fileExtension..") has been dropped into the tool.")
+  if fileExtension == "png" or fileExtension == "jpg" then
+    assert(file:open("r")) local fileContent = file:read() file:close()
+    local Filedata, err = love.filesystem.newFileData(fileContent,fileName) if err then error(err) end
+    _Images[fileName] = love.graphics.newImage(Filedata)
+    _Status = "Loaded "..fileName..", Waiting for JSON file..."
+  elseif fileExtension == "json" then
+    assert(file:open("r")) local jData = file:read() file:close()
+    local jArr = JSON:decode(jData)
+  end
 end
 
 function love.update(dt)
